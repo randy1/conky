5a847ae1c1d71a27631645824b501c552274e321
Magliano Andrea <masterblaster@tiscali.it>
2011-04-14 17:36:27 +0200

    Preliminary import from 2.8.1 git release
    * don't use kvm_* calls, just sysctl (so no suid perm necessary)
    
    * sysctls calls in general aren't thread safe, collapse callbacks
      using same sysctls (specifically total/running procs and proc list
      ones).
    
      Some sysctls need two calls (first to get size of obj returned,
      second to get object self); if different threads use this schema
      on same sysctl, weird values are returned (first/second calls
      sequence should be serialized).
    
      In general it makes not much sense too having more threads that use
      the same sysctl; just get info once and populate all data.
    
    * add DragonFly specific extended uname string ($version in conky.conf)
      with git version and signature
    
    Todo:
    
    - top process list logic is old style, use top.cc funcs.
    - find a solution for cpu freq

--- src/conky.cc
+++ src/conky.cc
@@ -116,6 +116,8 @@
 #include "linux.h"
 #elif defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
 #include "freebsd.h"
+#elif defined(__DragonFly__)
+#include "dragonfly.h"
 #elif defined(__OpenBSD__)
 #include "openbsd.h"
 #endif
@@ -2778,7 +2780,7 @@ static void print_help(const char *prog_name) {
 
 inline void reset_optind() {
 #if defined(__FreeBSD__) || defined(__FreeBSD_kernel__) || defined(__OpenBSD__) \
-		|| defined(__NetBSD__)
+	|| defined(__NetBSD__) || defined(__DragonFly__)
 	optind = optreset = 1;
 #else
 	optind = 0;
@@ -3140,6 +3142,10 @@ int main(int argc, char **argv)
 		std::cerr << PACKAGE_NAME": " << e.what() << std::endl;
 		return EXIT_FAILURE;
 	}
+
+#if defined(__linux__)
+	disk_cleanup();
+#endif
 
 #if defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
 	kvm_close(kd);
